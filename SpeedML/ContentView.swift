1d25df92f7445c9c979ce8bee5a25749661bc2d3
Dominic Ndondo <dominic.ndondo@outlook.com>
2024-06-27 14:17:13 -0400

    Initial Commit

---
+++ SpeedML/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeedML
+//
+//  Created by DOMINIC NDONDO on 6/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeedML/SpeedMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedMLApp.swift
+//  SpeedML
+//
+//  Created by DOMINIC NDONDO on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SpeedMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
